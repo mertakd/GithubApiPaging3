34f43ee9a462e00ec790909f1c40e331dc8b1525
Mert Akdoğan2 <65607835+mertakd@users.noreply.github.com>
2024-01-14 19:27:07 +0300

    11. Displaying the loading state in Activity And empty list issue fixed const val IN_QUALIFIER = "in:name,description" const val IN_QUALIFIER = " in:name,description"

--- app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesActivity.kt
+++ app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesActivity.kt
@@ -77,7 +77,8 @@ class SearchRepositoriesActivity : AppCompatActivity() {
         val repoAdapter = ReposAdapter()
         list.adapter = repoAdapter.withLoadStateHeaderAndFooter(
             header = ReposLoadStateAdapter {repoAdapter.retry()},
-            footer = ReposLoadStateAdapter {repoAdapter.retry()}
+            footer = ReposLoadStateAdapter {repoAdapter.retry()
+            }
         )
 
         bindSearch(
@@ -137,6 +138,9 @@ class SearchRepositoriesActivity : AppCompatActivity() {
         pagingData: Flow<PagingData<Repo>>,
         onScrollChanged: (UiAction.Scroll) -> Unit
     ) {
+        //11. Displaying the loading state in Activity
+        retryButton.setOnClickListener { repoAdapter.retry() }
+
         list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 if (dy != 0) onScrollChanged(UiAction.Scroll(currentQuery = uiState.value.query))
@@ -168,6 +172,37 @@ class SearchRepositoriesActivity : AppCompatActivity() {
                 if (shouldScroll) list.scrollToPosition(0)
             }
         }
+
+        //11. Displaying the loading state in Activity
+        lifecycleScope.launch {
+            repoAdapter.loadStateFlow.collect { loadState ->
+                val isListEmpty = loadState.refresh is LoadState.NotLoading && repoAdapter.itemCount == 0
+                // show empty list
+                emptyList.isVisible = isListEmpty
+                // Only show the list if refresh succeeds.
+                list.isVisible = !isListEmpty
+
+
+                // Show loading spinner during initial load or refresh.
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                // Show the retry state if initial load or refresh fails.
+                retryButton.isVisible = loadState.source.refresh is LoadState.Error
+
+                // Toast on any error, regardless of whether it came from RemoteMediator or PagingSource
+                val errorState = loadState.source.append as? LoadState.Error
+                    ?: loadState.source.prepend as? LoadState.Error
+                    ?: loadState.append as? LoadState.Error
+                    ?: loadState.prepend as? LoadState.Error
+                errorState?.let {
+                    Toast.makeText(
+                        this@SearchRepositoriesActivity,
+                        "\uD83D\uDE28 Wooops ${it.error}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+
     }
 }
 
